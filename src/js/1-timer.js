c4e4c5b9e714fa6e1c49831281be5ae7ae897751
Vitalii <katrychvitalii23@gmail.com>
2026-08-19 16:31:28 +0300

    2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,84 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const chosenDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (chosenDate < currentDate) {
+      iziToast.show({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+
+      startBtn.disabled = true;
+      userSelectedDate = null;
+    } else {
+      startBtn.disabled = false;
+      userSelectedDate = chosenDate;
+      console.log('Обрано валідну дату:', userSelectedDate);
+    }
+
+    console.log(selectedDates[0]);
+  },
+};
+
+const startBtn = document.querySelector('[data-start]');
+
+let userSelectedDate = null;
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  const timerId = setInterval(() => {
+    const deltaTime = userSelectedDate - new Date();
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      console.log('Таймер зупинено');
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent =
+      addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent =
+      addLeadingZero(seconds);
+  }, 1000);
+});
+console.log('Починаємо зворотний відлік до:', userSelectedDate);
+
+startBtn.disabled = true;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.nextElementSibling.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: '`✅ Fulfilled promise in ${delay}ms`',
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: '`❌ Rejected promise in ${delay}ms`',
+        position: 'topRight',
+      });
+    });
+  form.reset();
+});
